00102270a38261b7a7a4d12453318d488f07e1eb
Jwhalen23 <120579598+Jwhalen23@users.noreply.github.com>
2026-03-17 15:00:27 -0500

    Create Student class with validation and points logic
    Implement Student class with name validation and points management.

---
+++ Student.java
@@ -0,0 +1,20 @@
+package campusQuest;
+
+public class Student {
+	private final int points;
+	private final String name;
+
+	public Student(String name) {
+		if (name == null || name == ""){
+			throw new IllegalArgumentException("Name isnt valid")}
+		this.name = name;
+		this.points = 0; 
+	}  // add points 
+	public void addPoints(int amount) { 
+		this.points = points + amount;
+		if(points < 0 ) {
+			throw new IllegalArgumentException("Points are negative")}
+		}
+	}
+
+}
