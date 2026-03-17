311200a5325b2fa7d88e95f693c89ac5262712f1
liamjolles <liamjolles@gmail.com>
2026-03-17 15:36:51 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
+package Proj;
+
 public class Student {
 	
 
@@ -10,6 +12,7 @@ public class Student {
 			throw new IllegalArgumentException("Name isnt valid");
 		}
 		
+		
 		this.name = name;
 		
 		this.points = 0; 
@@ -17,8 +20,8 @@ public class Student {
 	}  // add points 
 	public void addPoints(int amount) { 
 		
-		if(points < 0 ) {
-			throw new IllegalArgumentException("Points are negative");
+		if(amount < 0 ) {
+			throw new IllegalArgumentException("Amount must be positive");
 		}
 		
 		this.points = points + amount;
