326fcacbb02df0ee1ec3b93a73ac430abb7d3a61
liamjolles <liamjolles@gmail.com>
2026-03-17 15:29:09 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,19 +1,21 @@
-package campusQuest;
-
 public class Student {
-	private final int points;
-	private final String name;
+	
+
+	private int points;
+	private String name;
 
 	public Student(String name) {
+		
 		if (name == null || name == ""){
-			throw new IllegalArgumentException("Name isnt valid")}
+			throw new IllegalArgumentException("Name isnt valid");
+		}
 		this.name = name;
 		this.points = 0; 
 	}  // add points 
 	public void addPoints(int amount) { 
 		this.points = points + amount;
 		if(points < 0 ) {
-			throw new IllegalArgumentException("Points are negative")}
+			throw new IllegalArgumentException("Points are negative");
 		}
 	}
 
