374a7281266b6b7fe7417b4488237f3eec8f24ac
liamjolles <liamjolles@gmail.com>
2026-03-17 15:34:59 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,14 +9,19 @@ public class Student {
 		if (name == null || name == ""){
 			throw new IllegalArgumentException("Name isnt valid");
 		}
+		
 		this.name = name;
+		
 		this.points = 0; 
+		
 	}  // add points 
 	public void addPoints(int amount) { 
-		this.points = points + amount;
+		
 		if(points < 0 ) {
 			throw new IllegalArgumentException("Points are negative");
 		}
+		
+		this.points = points + amount;
 	}
 
 }
