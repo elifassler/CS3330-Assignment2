8a55ca8f7b87960d680cd182764374032a82d1b9
Eli Fassler <elijah.fassler@gmail.com>
2026-03-17 14:09:03 -0500

    Implement AbstractQuest class with validation

---
+++ AbstractQuest.java
@@ -0,0 +1,24 @@
+abstract class AbstractQuest implements Quest {
+	
+	private int id;
+	private String title;
+	private double basePoints;
+	private boolean completed;
+	
+	public AbstractQuest(int id, String title, double basePoints) {
+		if (id <= 0) {
+			throw new IllegalArgumentException("id has to be > 0");
+		}
+		if (title == null || title.isBlank()) {
+			throw new IllegalArgumentException("title cannot be null or blank");
+		}
+		if (basePoints <= 0) {
+			throw new IllegalArgumentException("basePoints has to be > 0");
+		}
+		
+		this.id = id;
+		this.title = title;
+		this.basePoints = basePoints;
+		this.completed = false;
+	}
+}
