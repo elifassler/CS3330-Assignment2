d29a077acacd6ee22f1963874d448c9b4d748208
Eli Fassler <elijah.fassler@gmail.com>
2026-03-19 14:02:18 -0500

    Add validation and override methods in Student class

--- Student.java
+++ Student.java
@@ -9,7 +9,9 @@ public class Student {
 		if (name == null || name.isBlank()){
 			throw new IllegalArgumentException("Name isnt valid");
 		}
-		
+		if (points < 0){
+			throw new IllegalArgumentException("Points cannot be < 0");
+		}
 		
 		this.name = name;
 		
@@ -34,6 +36,31 @@ public class Student {
 	public String getName()
 	{
 		return name;
+	}
+
+	// Create our own equals() and hashCode()
+	// hashSet uses Object
+	@Override
+	public boolean equals(Object s) {
+		// Don't compare addresses
+		Student s1 = (Student) s;
+		
+		if (this == s) {
+			return true;
+		}
+		
+		return name.equals(s1.name);
+	}
+	
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		System.out.println("Name: " + name + ", Points: " + points);
+		return "";
 	}
 	
 }
