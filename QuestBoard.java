204a5dc7490e1ad68ee8adae9bf44d95e936bfc4
Eli Fassler <elijah.fassler@gmail.com>
2026-03-19 15:05:16 -0500

    Create QuestBoard class for quest management
    Implement QuestBoard class with quest management methods.

---
+++ QuestBoard.java
@@ -0,0 +1,67 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+// ALL THAT NEEDS TO BE DONE IS INVARIENTS FOR MOST METHODS
+
+public class QuestBoard {
+	
+	private Map<Integer, Quest> questsById;
+	private Map<Student, List<Quest>> assignments;
+	
+	public QuestBoard() {
+		questsById = new HashMap<>();
+		assignments = new HashMap<>();
+	}
+	
+	public void addQuest(Quest q) {
+		if (questsById.containsKey(q.getId())) {
+			throw new IllegalArgumentException("Duplicate id");
+		}
+	}
+	
+	public Quest findQuest(int id) {
+		return questsById.get(id);
+	}
+	
+	public void assignQuest(Student s, int questId) {
+		// First find the quest
+		Quest q = questsById.get(questId);
+		// Get quest list
+		List<Quest> studentQuests = assignments.get(s);
+		
+		// Decided not to be able to add same quest
+		if (!studentQuests.contains(q)) {
+			studentQuests.add(q);
+		}
+	}
+	
+	public int completeQuest(Student s, int questId) {
+		// Get quest from assignments
+		List<Quest> studentQuests = assignments.get(s);
+		// Search through the list and match id
+		for (Quest completedQuest : studentQuests) {
+			if (completedQuest.getId() == questId) {
+				return completedQuest.completeFor(s);
+			}
+		}
+		
+		throw new IllegalArgumentException("Couldn't find id");
+	}
+	
+	public void printAllQuests() {
+		// Iterate through all quests
+		for (Quest q : questsById.values()) {
+			System.out.println(q);
+		}
+	}
+	
+	public void printAssignmentsFor(Student s) {
+		List<Quest> studentQuests = assignments.get(s);
+		
+		// Iterate through all student quests
+		for (Quest q : studentQuests) {
+			System.out.println(q);
+		}
+	}
+}
